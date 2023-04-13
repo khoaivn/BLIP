210605c60483db8bfab6021d3f4c30eda78de18d
phamgi <trungdunght2003@gmail.com>
2023-04-13 15:59:39 +0700

    final follow clip

--- dungpt/gadients_numpy.py
+++ dungpt/gadients_numpy.py
@@ -2,29 +2,51 @@ import torch
 import torch.nn as nn
 
 
-X = torch.tensor([1, 2, 3, 4], dtype=torch.float32)
-Y = torch.tensor([2, 4, 6, 8], dtype=torch.float32)
+X = torch.tensor([[1], [2], [3], [4]], dtype=torch.float32)
+Y = torch.tensor([[2], [4], [6], [8]], dtype=torch.float32)
 
-w = torch.tensor(0.0, dtype = torch.float32, requires_grad = True)
+# w = torch.tensor(0.0, dtype = torch.float32, requires_grad = True)
 
-def forward(x):
-    return w * x
+# def forward(x):
+#     return w * x
 
-print(f'Prediction before training: f(5) = {forward(5):.3f}')
+X_test = torch.tensor([5], dtype=torch.float32)
+
+n_samples, n_features = X.shape
+# print(n_samples, n_features)
+
+input_size = n_features
+output_size = n_features
+
+# model = nn.Linear(input_size, output_size)
+
+class LinearRegression(nn.Module):
+    def __init__(self, input_dim, output_dim):
+        super(LinearRegression, self).__init__()
+        self.lin = nn.Linear(input_dim, output_dim)
+    
+    def forward(self, x):
+        return self.lin(x)
+
+model = LinearRegression(input_size, output_size)
+
+# print(model(X_test).item())
+print(f'--Prediction before training: f(5) = {model(X_test).item():.3f}')
 
 learning_rate = 0.01
 n_iters = 100
 
 loss = nn.MSELoss()
-optimizer = torch.optim.SGD([w], lr=learning_rate)
+optimizer = torch.optim.SGD(model.parameters(), lr=learning_rate)
 
 for epoch in range(n_iters):
-    y_pred = forward(X)
+    y_pred = model(X)
     l = loss(Y, y_pred)
     l.backward()
     optimizer.step()
     optimizer.zero_grad()
     if (epoch % 10 == 0):
-        print(f'epoch {epoch+1}: w = {w:.3f}, loss = {l:.0f}')
+        [w, b] = model.parameters()
+        print(f'epoch {epoch+1}: w = {w[0][0].item():.3f}, loss = {l:.4f}')
 
-print(f'Prediction before training: f(5) = {forward(5):.3f}')
+print(f'Prediction before training: f(5) = {model(X_test).item():.3f}')
